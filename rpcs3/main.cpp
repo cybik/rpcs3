8b35154ea0c6f96bc820b1183c5e179968593ab0
flash-fire <mudroch.robert@gmail.com>
2017-04-12 17:15:29 -0500

    Mae build work with visual studio and Qt.  Qt stuff is now replacing the wx code for default to launch.
    Linux doesn't work as cmake isn't set yet. Sorry.

---
+++ rpcs3/main.cpp
@@ -0,0 +1,23 @@
+// Qt5.2+ frontend implementation for rpcs3. Known to work on Windows, Linux, Mac
+// by Sacha Refshauge
+#ifdef QT_UI
+
+#include <QApplication>
+
+#include "GUI/mainwindow.h"
+
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	MainWindow mainWin;
+	QSize defaultSize = QSize(800, 600);
+	mainWin.resize(defaultSize);
+
+	mainWin.show();
+	
+	return app.exec();
+}
+
+#endif // QT_UI
